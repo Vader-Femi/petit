a0061121d81c4c6a916b8c4a592d077bbc8f1a43
Femi <oluwafemi.za@gmail.com>
2025-01-11 22:16:30 +0100

    Day 83 of 100
    Today I implemented the file picker and file compression. I only tested the single file picker, and I still haven't thought of a UI for the app.
    
    I compress an image from 7.4MB to 567kb. With 50% resolution and 90% quality

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:gallery_saver_plus/gallery_saver.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -16,12 +20,20 @@ class HomePage extends StatelessWidget {
           children: [
             Spacer(),
             Text(
-              "We'll continue from here",
+              "The selected file will show here",
               style: TextStyle(fontSize: 18),
             ),
             Spacer(),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () async {
+                var pickedFile = await _pickSingleFile(context);
+                var compressedFile =
+                    await compressImage(file: pickedFile, quality: 90);
+                if (compressedFile != null) {
+                  _saveLocalImage(
+                      context: context, compressedImage: compressedFile);
+                }
+              },
               child: const Text('Pick a File'),
             ),
           ],
@@ -29,4 +41,71 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
+  Future<File> _pickSingleFile(BuildContext context) async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      PlatformFile file = result.files.first;
+      File pickedFile = File(file.path!);
+      return File(pickedFile.path);
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("No file picked!")));
+
+      throw Exception('No file picked');
+    }
+  }
+
+  Future<List<File>> _pickMultipleFiles(BuildContext context) async {
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(allowMultiple: true);
+
+    if (result != null) {
+      return result.paths.map((path) => File(path!)).toList();
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("No file picked!")));
+
+      throw Exception('No file picked');
+    }
+  }
+
+  Future<XFile?> compressImage({
+    required File file,
+    required int quality,
+    int minWidth = 1000,
+    int minHeight = 1000,
+  }) async {
+    final filePath = file.absolute.path;
+    final lastIndex = filePath.lastIndexOf(RegExp(r'.jp'));
+    final splitted = filePath.substring(0, (lastIndex));
+    final outPath = "${splitted}_out${filePath.substring(lastIndex)}";
+
+    return await FlutterImageCompress.compressAndGetFile(filePath, outPath,
+        minWidth: minWidth, minHeight: minHeight, quality: quality);
+  }
+
+  Future<void> _saveLocalImage({
+    required BuildContext context,
+    required XFile compressedImage,
+  }) async {
+    try {
+      GallerySaver.saveImage(compressedImage.path);
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Image Saved to Galery!")));
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Error ocurred - ${e.toString()}")));
+    }
+  }
 }
+
+// List<File> compressedFiles = List.empty();
+//
+// for (var file in files) {
+// var compressed = await compressImage(file: file, quality: quality);
+// compressedFiles.add(File(compressed!.path));
+// }
+//
+// return compressedFiles;
